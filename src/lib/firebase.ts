719447e5eeda959f4b9952dd02df67bde5366fe5
koushikrad <koushikrad@gmail.com>
2025-11-23 12:40:14 +0800

    refactor: externalize Firebase configuration to environment variables and ignore .env file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJTOiO-1iBq7t-C_p8A3IlSgobSQZ_7uE",
-    authDomain: "my-portfolio-92483.firebaseapp.com",
-    projectId: "my-portfolio-92483",
-    storageBucket: "my-portfolio-92483.firebasestorage.app",
-    messagingSenderId: "406713543010",
-    appId: "1:406713543010:web:3c12ca7f8c962a1f4aa43b",
-    measurementId: "G-K3WFFXM2E2"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
